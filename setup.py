262da726f1209ce41da8ef0d5a05205c3663eb79
Artur von Ruffer <avonruffer@ethz.ch>
2023-11-09 18:14:05 +0100

    Add calibration_node.py

--- setup.py
+++ setup.py
@@ -13,13 +13,14 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='aidara',
-    maintainer_email='aidara@todo.todo',
-    description='TODO: Package description',
+    maintainer='Artur von Ruffer',
+    maintainer_email='avonruffer@ethz.ch',
+    description='Publishes transformation from camera to checkerboard frame',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'calibration_node = camera_calibration.calibration_node:main'
         ],
     },
 )
